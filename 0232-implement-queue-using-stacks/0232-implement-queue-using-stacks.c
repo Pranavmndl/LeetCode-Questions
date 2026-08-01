485b75b014cefa46092de13dfd78afa5c84b4527
Pranav Kumar <pranavmndl9@gmail.com>
2026-08-01 22:57:43 +0530

    Time: 0 ms (100%), Space: 9.1 MB (95.45%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.c
@@ -0,0 +1,68 @@
+
+
+
+typedef struct {
+    int stack1[100];
+    int stack2[100];
+    int top1;
+    int top2;
+    
+} MyQueue;
+
+
+MyQueue* myQueueCreate() {
+    MyQueue *obj=(MyQueue *)malloc(sizeof(MyQueue));
+    obj->top1=-1;
+    obj->top2=-1;
+    return obj;
+    
+}
+
+void myQueuePush(MyQueue* obj, int x) {
+    obj->stack1[++obj->top1]=x;
+    
+}
+
+int myQueuePop(MyQueue* obj) {
+    if(obj->top2==-1){
+        while(obj->top1 !=-1){
+            obj->stack2[++obj->top2]=obj->stack1[obj->top1--];
+        }
+    }
+    return obj->stack2[obj->top2--];
+    
+}
+
+int myQueuePeek(MyQueue* obj) {
+    if(obj->top2==-1){
+        while(obj->top1 !=-1){
+            obj->stack2[++obj->top2]=obj->stack1[obj->top1--];
+        }
+    }
+    return obj->stack2[obj->top2];
+    
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+    return(obj->top1==-1 && obj->top2==-1);
+    
+}
+
+void myQueueFree(MyQueue* obj) {
+    free(obj);
+    
+}
+
+/**
+ * Your MyQueue struct will be instantiated and called as such:
+ * MyQueue* obj = myQueueCreate();
+ * myQueuePush(obj, x);
+ 
+ * int param_2 = myQueuePop(obj);
+ 
+ * int param_3 = myQueuePeek(obj);
+ 
+ * bool param_4 = myQueueEmpty(obj);
+ 
+ * myQueueFree(obj);
+*/
