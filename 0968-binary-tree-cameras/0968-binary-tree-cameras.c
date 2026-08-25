978e539b2ff0441220fb04aa0f9edd5ea657eafd
Pranav Kumar <pranavmndl9@gmail.com>
2026-08-25 23:05:54 +0530

    Time: 0 ms (100%), Space: 11.8 MB (47.83%) - LeetHub

---
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int cameras=0;
+int dfs(struct TreeNode* root){
+    if (root==NULL)
+        return 1;
+    int left=dfs(root->left);
+    int right=dfs(root->right);
+    if(left==0||right==0){
+        cameras++;
+        return 2;
+    }
+    if (left==2||right==2)
+        return 1;
+    return 0;
+}
+int minCameraCover(struct TreeNode* root){
+    cameras=0;
+    if(dfs(root)==0)
+        cameras++;
+    return cameras;
+}
